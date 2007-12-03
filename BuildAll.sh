391aee89a1ae6e7a0397032c12168c0cb371ea3e
Robert Heller <heller@deepsoft.com>
2007-12-03 16:54:50 +0000

    Prepare for conversion to subversion

--- BuildAll.sh
+++ BuildAll.sh
@@ -2,5 +2,5 @@
 #./bootstrap
 #./configure --with-tclkitrun=/usr/local/share/tclkits/tclkit-linux-x86
 make PLATFORM=LinuxX86 bintgz
-make PLATFORM=Win32 EXEEXT=.exe TCLKITRUN=/usr/local/share/tclkits/tclkit-win32.exe binzip
+make PLATFORM=Win32 TCLXMLLIB=../Tclxml2.6Win32 EXEEXT=.exe TCLKITRUN=/usr/local/share/tclkits/tclkit-win32.exe binzip
 make dist
